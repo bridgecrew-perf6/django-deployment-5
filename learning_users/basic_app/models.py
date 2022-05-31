e798dc86a2368b7535029a80a76612b96382befa
zrg444 <zachgorecznyfour4@gmail.com>
2022-05-31 14:30:31 -0400

    first commit

---
+++ learning_users/basic_app/models.py
@@ -0,0 +1,13 @@
+from pyexpat import model
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    potfolio_site = models.URLField(blank=True)
+    pro_pic = models.ImageField(upload_to="profile_pics", blank=True)
+    
+    def __str__(self):
+        return self.user.username
